27e02608ed0f3cd455aa2622a47d8aedb57289dd
SorayaBoolean <sorayakhyabaniassl@gmail.com>
2022-09-12 17:10:55 +0200

    add: Created first starting structure with an array of students in which each student has name, lastname and an array of votes

---
+++ Snack7.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Media voti Alinnu</title>
+</head>
+<body>
+
+    <?php
+
+    $alunni = [
+
+        ['nome'=> 'Mario',
+         'cognome'=> 'Geracitano',
+         'voti'=> [
+            'voto1' => 2,
+            'voto1' => 10,
+            'voto1' => 6,
+         ]
+         ],
+         ['nome'=> 'Sofia',
+         'cognome'=> 'Rossi',
+         'voti'=> [
+            'voto1' => 8,
+            'voto1' => 6,
+            'voto1' => 6,
+         ]
+         ],
+         ['nome'=> 'Luca',
+         'cognome'=> 'Fanci',
+         'voti'=> [
+            'voto1' => 6,
+            'voto1' => 7,
+            'voto1' => 7,
+         ]
+         ],
+         ['nome'=> 'Beatrice',
+         'cognome'=> 'Sarri',
+         'voti'=> [
+            'voto1' => 7,
+            'voto1' => 10,
+            'voto1' =>9,
+         ]
+         ],
+         ['nome'=> 'Davide',
+         'cognome'=> 'Baglioni',
+         'voti'=> [
+            'voto1' => 8,
+            'voto1' => 10,
+            'voto1' => 10,
+         ]
+         ]
+
+    ]
+
+
+
+    ?>
+
+    
+</body>
+</html>
